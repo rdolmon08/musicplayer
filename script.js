d6df21ccfcfdfa413589754981deedf5675d6124
Rebecca D. <rdolmon08@gmail.com>
2024-08-16 13:28:17 -0500

    adding event listeners

--- script.js
+++ script.js
@@ -87,7 +87,18 @@ let userData = {
 
 const playSong = (id) => {
   const song = userData?.songs.find((song) => song.id === id);
+  audio.src = song.src;
+  audio.title = song.title;
 
+  if (userData?.currentSong === null || userData?.currentSong.id !== song.id) {
+    audio.currentTime = 0;
+  } else {
+    audio.currentTime = userData?.songCurrentTime;
+  }
+  userData.currentSong = song;
+  playButton.classList.add("playing");
+
+  audio.play();
 };
 
 const renderSongs = (array) => {
@@ -112,6 +123,10 @@ const renderSongs = (array) => {
   playlistSongs.innerHTML = songsHTML;
 };
 
+playButton.addEventListener("click", () => {
+
+});
+
 const sortSongs = () => {
   userData?.songs.sort((a,b) => {
     if (a.title < b.title) {
@@ -128,4 +143,5 @@ const sortSongs = () => {
   return userData?.songs;
 };
 
-renderSongs(sortSongs());
+
+renderSongs(userData?.songs);
